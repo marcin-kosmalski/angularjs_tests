0f5c167f51fc7326d9deb679dc7efca840a4788a
marcin-kosmalski <marcin.kosmalski@gmail.com>
2015-10-31 20:58:52 +0100

    1

---
+++ app/test/my.js
@@ -0,0 +1,18 @@
+(function() {
+	'use strict';
+
+	angular.module('myApp.my',[]).controller('MyController',MyController);
+
+	function MyController() {
+		
+		var vm = this;
+		
+		vm.name = {};
+		
+		vm.sendMessage = function() {
+			
+		};
+		
+		
+	}
+})();
